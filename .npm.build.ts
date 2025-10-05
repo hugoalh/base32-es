3320ac0d32ab31fc6d6483313beb6e787546e369
hugoalh <hugoalh@gmail.com>
2025-10-05 17:03:09 +0800

    Update resources

--- .npm.build.ts
+++ .npm.build.ts
@@ -31,10 +31,6 @@ await invokeDenoNodeJSTransformer({
 			type: "git",
 			url: "git+https://github.com/hugoalh/base32-es.git"
 		},
-		scripts: {
-		},
-		engines: {
-		},
 		private: false,
 		publishConfig: {
 			access: "public"
